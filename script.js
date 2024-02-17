ff4320bfa62bce9b1df1c9240ec8bfc8fc9cb245
ServalFake <133217496+ServalFake@users.noreply.github.com>
2024-02-17 03:57:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// array of all pals
+const pals = [penking, pyrinnoct, mossanda, 
+    mossandalux, nitewing, incineram, 
+    incineramnoct, cinnamoth, elizabee, 
+    grintale, sweepa, univolt,
+    foxcicle, rayhound, surfent, surfentterra, tomebat, vanwyrm,
+    wanwyrmcryst, bushi, beakon, ragnahawk, katress, sibelyx,
+    elphidran, elphidranaqua, azurobe, blazehowl, blazehowlnoct, 
+    relaxaurus, relaxauruslux, reptyro,
+    kingpaca, icekingpaca, mammorest, mammorestcryst, 
+    wumpo, helzephyr, anubis,
+    icereptyro, menasting, lyleen, warsect, quiver, 
+    jormuntideignis, pyrin, helezephyr, grizzbolt, faleris, paladius,
+    necromus, frostallionnoct, jetragon, blazamut, 
+    suzakuaqua, lyleennoct, cryolinx, orserk,
+    shadowbeak, jormuntide, wumpobotan]
+    // array of individual pals
+    penking = ['penking', 11, 520]
+    pyrinnoct = ['pyrinnoct', 58, 240]
